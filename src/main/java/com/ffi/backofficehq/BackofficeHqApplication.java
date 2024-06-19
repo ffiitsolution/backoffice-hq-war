56669e8934adfe03dbfeaf5871715a2af8f9b6b4
Sifa Salafiah <sifa.salafiah@gmail.com>
2024-06-19 09:00:13 +0700

    chore: handle cors

--- src/main/java/com/ffi/backofficehq/BackofficeHqApplication.java
+++ src/main/java/com/ffi/backofficehq/BackofficeHqApplication.java
@@ -2,8 +2,10 @@ package com.ffi.backofficehq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,5 +15,25 @@ public class BackofficeHqApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackofficeHqApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerImpl();
+    }
+
+    private static class WebMvcConfigurerImpl implements WebMvcConfigurer {
+
+        public WebMvcConfigurerImpl() {
+        }
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(false);
+        }
+    }
 
 }
